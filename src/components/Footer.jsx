332f0619619f2360b8705bfbfb91bf55999edba8
perry-cyber <victorsimon482@gmail.com>
2025-03-29 13:21:07 -0600

    new add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,17 @@ import {
   FaInstagram,
   FaLinkedinIn,
 } from "react-icons/fa";
+import DashboardFooter from "./DashboardFooter";
+import logo from '../assets/loanbills-removebg-preview.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white px-6 md:px-20 py-12">
+    <footer className="bg-black border-t-white text-white px-6 md:px-20 py-12">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10">
         {/* Logo & Description */}
         <div className="md:col-span-1">
           <div className="flex items-center gap-2 mb-4">
-            <div className="bg-yellow-400 w-10 h-10 rotate-45"></div>
+             <img src={logo} alt="Company Logo" className="h-8 w-8 sm:h-10 sm:w-10 mr-3" />
             <h1 className="text-2xl font-bold">
               LoanBills<span className="tracking-wide">FX</span>
             </h1>
@@ -25,7 +27,6 @@ const Footer = () => {
           </p>
         </div>
 
-
         {/* Utility Page */}
         <div>
           <h3 className="text-lg font-semibold mb-4">Services</h3>
@@ -35,6 +36,14 @@ const Footer = () => {
             <li>Currency Exchange</li>
             <li>International Transfers</li>
             <li>Terms & Conditions</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="text-lg font-semibold mb-4">Contact</h3>
+          <ul className="space-y-2 text-gray-300">
+            <li>Address: plot 12 bannex plaza, Kubwa abuja</li>
+            
           </ul>
         </div>
       </div>
@@ -59,7 +68,9 @@ const Footer = () => {
           </a>
         </div>
       </div>
+      <DashboardFooter/>
     </footer>
+    
   );
 };
 
